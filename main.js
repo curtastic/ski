834d105f1a22c2a3e487f68b83a18a2d9925a2b5
Curtis Robinson <curtastic@gmail.com>
2023-07-19 09:58:17 -0700

    Timer, pause menu, reset game.

--- main.js
+++ main.js
@@ -7,8 +7,9 @@ var w = window,
 	gStateLoops = 0,
 	gCamX = 0,
 	gCamY = 0,
-	gYou = {x:9,y:1,z:0,speed:0,speedMax:.09,angle:1.57,frame:0,trailY:0},
+	gYou = {},
 	gSize = 16,
+	gButtonSize = 48,
 	gSizeX = 0,
 	gSizeY = 0,
 	gTilesYMin = 19,
@@ -16,6 +17,9 @@ var w = window,
 	gTilesY = 0,
 	gScale = 1,
 	gMobile,
+	gKeyDown = [],
+	gKeyHit = [],
+	gKeyReleased = [],
 	gMouseDown,
 	gMouseHit,
 	gMouseReleased,
@@ -34,11 +38,26 @@ var w = window,
 	gTrails=[],
 	gGuys=[],
 	gPi = Math.PI,
+	gStartTime = 0,
+	gPauseTime = 0,
+	gPauseTimeTotal = 0,
 	u
 gGuys.push(gYou)
 
 var gStateSet = (state) => {
 	gLog("stateSet() from",gState,"to",state)
+	
+	if(state == 'paused') {
+		gPauseTime = gloop.time
+	}
+	if(state == 'playing') {
+		if(gState == 'paused') {
+			gPauseTimeTotal += gloop.time-gPauseTime
+		} else {
+			gStartTime = gloop.time
+		}
+	}
+	
 	gState = state
 	gStateLoop = gloop.updates
 	gStateLoops = 0
@@ -49,48 +68,57 @@ var gGameUpdate = () => {
 	
 	gInputUpdate()
 
-	for(var guy of gGuys) {
-		guy.moved = 0
-
-		if(guy == gYou) {
-			if(gMouseDown) {
-				gYou.going = 1
-				if(gYou.z) {
-					gYou.z = 0
-					gYou.frame = 0
+	if(gState == 'playing') {
+		for(var guy of gGuys) {
+			guy.moved = 0
+	
+			if(guy == gYou) {
+				if(gMouseDown) {
+					gYou.going = 1
+					if(gYou.z) {
+						gYou.z = 0
+						gYou.frame = 0
+					}
+					//gYou.angle += .02*(gYou.x-gMouseTileX)
+					var angle = Math.atan2(Math.max(0,gMouseTileY-gYou.y), gMouseTileX-gYou.x)
+					gLog(angle, gYou.angle)
+					if(angle < 0)angle = 0
+					if(angle > gPi)angle = gPi
+					gYou.angle += Math.sign(angle-gYou.angle)*.05
 				}
-				//gYou.angle += .02*(gYou.x-gMouseTileX)
-				var angle = Math.atan2(Math.max(0,gMouseTileY-gYou.y), gMouseTileX-gYou.x)
-				gLog(angle, gYou.angle)
-				if(angle < 0)angle = 0
-				if(angle > gPi)angle = gPi
-				gYou.angle += Math.sign(angle-gYou.angle)*.05
+				if(gYou.angle<gPi*.1)gYou.angle=gPi*.1
+				if(gYou.angle>gPi*.9)gYou.angle=gPi*.9
+				var oldX = gYou.x
+				var goX = Math.cos(gYou.angle)*guySpeedGet(gYou)
+				gYou.x += goX
+				if(gHitGrid(gYou.x+.2+(goX>0)*.6, gYou.y+.4, gYou.z) || gHitGrid(gYou.x+.2+(goX>0)*.6, gYou.y+.9, gYou.z)) {
+					gYou.x = oldX
+					//gYou.angle = gPi/2
+					//gYou.speed = 0
+					gYou.z = 0
+				} else {
+					guy.moved = 1
+				}
+				
+				if(gMouseReleased) {
+					gYou.z = 1
+				}
 			}
-			if(gYou.angle<gPi*.1)gYou.angle=gPi*.1
-			if(gYou.angle>gPi*.9)gYou.angle=gPi*.9
-			var oldX = gYou.x
-			var goX = Math.cos(gYou.angle)*guySpeedGet(gYou)
-			gYou.x += goX
-			if(gHitGrid(gYou.x+.2+(goX>0)*.6, gYou.y+.4, gYou.z) || gHitGrid(gYou.x+.2+(goX>0)*.6, gYou.y+.9, gYou.z)) {
-				gYou.x = oldX
-				//gYou.angle = gPi/2
-				//gYou.speed = 0
-				gYou.z = 0
-			} else {
-				guy.moved = 1
+			if(guy.going)
+				gGuyGoDown(guy)
+			if(guy.moved) {
+				guy.frame+=.1
+				if(~~guy.frame>1)guy.frame-=2
 			}
-			
-			if(gMouseReleased) {
-				gYou.z = 1
-			}
-		}
-		if(guy.going)
-			gGuyGoDown(guy)
-		if(guy.moved) {
-			guy.frame+=.1
-			if(~~guy.frame>1)guy.frame-=2
 		}
 	}
+
+	if(gKeyHit[27]) {
+		gStateSet(gState == 'paused' ? 'playing':'paused')
+	}
+	
+	gKeyHit = []
+	gKeyReleased = []
 }
 
 var gGuyGoDown = (guy) => {
@@ -136,17 +164,13 @@ var gInputUpdate = () => {
 var gGridIn = (x,y) => x>=0 && y>=0 && x<gGrid.length && y<gGrid[0].length
 
 var gGridDraw = (layer) => {
-	var tilesX = gTilesX + 1, tilesY = gTilesY+1
+	var tilesX = gTilesX + 1, tilesY = gTilesY+2
 	gCamX = gYou.x - gSizeX/2/gSize
 	gCamY = gYou.y - gSizeY/2/gSize
 	for(var y=0; y<tilesY; y++) {
 		for(var x=0; x<tilesX; x++) {
 			var gridX = x + gCamX
 			var gridY = y + gCamY
-			var addX = -(gCamX % 1) * gSize
-			var addY = -(gCamY % 1) * gSize
-			if(gCamX<0)addX-=gSize
-			if(gCamY<0)addY-=gSize
 			gridX = Math.floor(gridX)
 			gridY = Math.floor(gridY)
 			if(gGridIn(gridX, gridY)) {
@@ -156,6 +180,10 @@ var gGridDraw = (layer) => {
 			}
 			if((kind.id=='T')==layer)
 			if(kind.image) {
+				var addX = -(gCamX % 1) * gSize
+				var addY = -(gCamY % 1) * gSize
+				if(gCamX<0)addX-=gSize
+				if(gCamY<0)addY-=gSize
 				var drawX = x*gSize+addX
 				var drawY = y*gSize+addY
 				gl1.imageDraw(kind.image, drawX, drawY+kind.offsetY)
@@ -165,7 +193,7 @@ var gGridDraw = (layer) => {
 
 	var x = 0
 	while(x<gSizeX) {
-		gl1.imageDraw(gCloudImage,x,tilesY*gSize-29)
+		gl1.imageDraw(gCloudImage,x,(tilesY-2)*gSize-13)
 		x += gCloudImage.sizeX
 	}
 }
@@ -218,9 +246,56 @@ var gGameDraw = () => {
 		}
 	}
 
+	if(gState == 'paused') {
+		gl1.drawRect(0,0,gSizeX,gSizeY,0x33)
+		glText.draw("PAUSED", gSizeX/2, gSizeY*.3, 2, 1)
+
+		var x = gSizeX/2-gSize*3
+		var y = gSizeY*.5
+		var onBox = gMouseOnBox(x,y,gButtonSize,gButtonSize) && !gMouseDragged
+		gl1.imageDraw(gMouseDown && onBox?gResetButtonDown:gResetButton,x,y,gButtonSize,gButtonSize)
+		if(gMouseClicked && onBox) {
+			gReset()
+		}
+		var x = gSizeX/2+gSize
+		var y = gSizeY*.5
+		var onBox = gMouseOnBox(x,y,gButtonSize,gButtonSize) && !gMouseDragged
+		gl1.imageDraw(gMouseDown && onBox?gPlayButtonDown:gPlayButton,x,y,gButtonSize,gButtonSize)
+		if(gMouseClicked && onBox) {
+			gStateSet('playing')
+		}
+	}
+
+	var size=32
+	var onBox = gMouseOnBox(4,4,size,size) && !gMouseDragged
+	gl1.imageDraw(gMouseDown && onBox?gPauseButtonDown:gPauseButton,4,4,size,size)
+	if(gMouseClicked && onBox) {
+		gStateSet(gState == 'paused' ? 'playing':'paused')
+	}
+
+	var time = gloop.time - gStartTime - gPauseTimeTotal
+	if(gState == 'paused') {
+		time -= gloop.time - gPauseTime
+	}
+	var sec = time/1000 | 0
+	var min = sec/60 | 0
+	sec = sec % 60 + ''
+	if(sec.length<2)sec='0'+sec
+	glText.draw(min+":"+sec, gSizeX/2, 2, 1, 1)
+
 	gl1.render()
 
 	gMouseClicked = gMouseHit = gMouseReleased = 0
+}
+
+var gMouseDownOnBox = (x,y,sizeX,sizeY) => {
+	return gMouseDown && !gMouseDragged && gMouseOnBox(x,y,sizeX,sizeY)
+}
+
+var gMouseOnBox = (x,y,sizeX,sizeY) => {
+	var mouseX = gMouseX/gScale
+	var mouseY = gMouseY/gScale
+	return mouseX>=x && mouseY>=y && mouseX<=x+sizeX && mouseY<=y+sizeY
 }
 
 var gMouseMove = () => {
@@ -240,6 +315,13 @@ w.onload = () => {
 	w.gPlayerImage2 = gl1.imageMake16(11, 5)
 	w.gPlayerTrailImage = gl1.imageMake(10*gSize, 4*gSize,gSize,3)
 	w.gPlayerTrailImage2 = gl1.imageMake(11*gSize, 4*gSize,gSize,3)
+	
+	w.gPauseButton = gl1.imageMake16(12, 0)
+	w.gPauseButtonDown = gl1.imageMake16(13, 0)
+	w.gPlayButton = gl1.imageMake16(12, 1)
+	w.gPlayButtonDown = gl1.imageMake16(13, 1)
+	w.gResetButton = gl1.imageMake16(12, 2)
+	w.gResetButtonDown = gl1.imageMake16(13, 2)
 
 	var kind = {id:' ', name:'path',texX:2.5,texY:2.5}
 	gTileKinds.push(kind)
@@ -258,6 +340,119 @@ w.onload = () => {
 		kind.texSizeY = kind.texSizeY||1
 		kind.image = gl1.imageMake(kind.texX*gSize, kind.texY*gSize, gSize, gSize*kind.texSizeY)
 	}
+	
+	onresize = _ => {
+		var screenRatio = innerWidth/innerHeight
+		gTilesY = gTilesYMin
+		gSizeY = gTilesY*gSize
+		gScale = innerHeight / gSizeY | 0
+		gSizeX = innerWidth / gScale | 0
+		gTilesX = gSizeX / gSize
+		
+		gGameCanvas.setAttribute('width', ~~gSizeX)
+		gGameCanvas.setAttribute('height', ~~gSizeY)
+
+		gGameCanvas.style.width = ~~(gSizeX*gScale)+'px'
+		gGameCanvas.style.height = ~~(gSizeY*gScale)+'px'
+		
+		gl1.resize()
+	}
+
+	ondragstart = _ => false 
+	onselectstart = _ => false 
+	
+	addEventListener("mousedown", e => {
+		gMouseDown = 1
+		gMouseHit = 1
+		gMouseDragged = 0
+		gMouseX = e.pageX
+		gMouseY = e.pageY
+		gMouseDownX = gMouseX
+		gMouseDownY = gMouseY
+	})
+	
+	addEventListener("mousemove", e => {
+		gMouseX = e.pageX
+		gMouseY = e.pageY
+		gMouseMove()
+	})
+	
+	document.addEventListener("touchstart", e => {
+		gMouseDown = 1
+		gMouseHit = 1
+		gMouseDragged = 0
+		for(var touchI=-1,touch; touch=e.changedTouches[++touchI];) {
+			gMouseX = touch.clientX
+			gMouseY = touch.clientY
+		}
+		gMouseDownX = gMouseX
+		gMouseDownY = gMouseY
+		e.preventDefault()
+		return false
+	}, {passive:false})
+	
+	document.addEventListener("touchmove", e => {
+		for(var touchI=-1,touch; touch=e.changedTouches[++touchI];) {
+			gMouseX = touch.clientX
+			gMouseY = touch.clientY
+		}
+		gMouseMove()
+		e.preventDefault()
+		return false
+	}, {passive:false})
+	
+	document.addEventListener("touchend", e => {
+		gMouseDown = 0
+		gMouseReleased = 1
+		gMouseClicked = !gMouseDragged
+		e.preventDefault()
+		return false
+	}, {passive:false})
+	
+	document.addEventListener("touchcancel", e => {
+		gMouseDown = 0
+		gMouseReleased = 1
+		e.preventDefault()
+		return false
+		
+	}, {passive:false})
+	
+	addEventListener("mouseup", e => {
+		gMouseDown = 0
+		gMouseReleased = 1
+		gMouseClicked = !gMouseDragged
+	})
+	
+	addEventListener("keydown", e => {
+		var c = e.keyCode
+		if(!gKeyDown[c]) {
+			gKeyHit[c] = gKeyDown[c] = 1
+		}
+	})
+	
+	addEventListener("keyup", e => {
+		var c = e.keyCode
+		gKeyDown[c] = 0
+		gKeyReleased[c] = 1
+	})
+	
+	onresize()
+
+	gReset()
+	
+	gloop.start(gGameUpdate, gGameDraw)
+}
+
+var gReset = () => {
+	gYou.x = 9
+	gYou.y = 1
+	gYou.z = 0
+	gYou.speed = 0
+	gYou.speedMax = .09
+	gYou.angle = 1.57
+	gYou.frame = 0
+	gYou.trailY = 0
+	
 	var grid = `
 ......      ......
 ......      ......
@@ -329,101 +524,20 @@ tt....      ......
 	grid = grid.split('\n')
 	grid.pop()
 	grid.shift()
-	for (let x=0; x<grid[0].length; x++) {
+	for (let x=0; x<=grid[0].length; x++) {
 		gGrid[x] = []
 	}
 	
 	for(var y=0; y<grid.length; y++) {
-		var row = grid[y]
+		var row = 'T'+grid[y]
 		for (let x=0; x<row.length; x++) {
 			gGrid[x][y] = gTileKindsById[row[x]]
 		}
 	}
-	
-	w.onresize = _ => {
-		var screenRatio = innerWidth/innerHeight
-		gTilesY = gTilesYMin
-		gSizeY = gTilesY*gSize
-		gScale = innerHeight / gSizeY | 0
-		gSizeX = innerWidth / gScale | 0
-		gTilesX = gSizeX / gSize
-		
-		gGameCanvas.setAttribute('width', ~~gSizeX)
-		gGameCanvas.setAttribute('height', ~~gSizeY)
 
-		gGameCanvas.style.width = ~~(gSizeX*gScale)+'px'
-		gGameCanvas.style.height = ~~(gSizeY*gScale)+'px'
-		
-		gl1.resize()
+	if(gState != 'loading') {
+		gStateSet('title')
 	}
-
-	w.ondragstart = _ => false 
-	w.onselectstart = _ => false 
 	
-	w.addEventListener("mousedown", e => {
-		gMouseDown = 1
-		gMouseHit = 1
-		gMouseDragged = 0
-		gMouseX = e.pageX
-		gMouseY = e.pageY
-		gMouseDownX = gMouseX
-		gMouseDownY = gMouseY
-	})
-	
-	w.addEventListener("mousemove", e => {
-		gMouseX = e.pageX
-		gMouseY = e.pageY
-		gMouseMove()
-	})
-	
-	document.addEventListener("touchstart", e => {
-		gMouseDown = 1
-		gMouseHit = 1
-		gMouseDragged = 0
-		for(var touchI=-1,touch; touch=e.changedTouches[++touchI];) {
-			gMouseX = touch.clientX
-			gMouseY = touch.clientY
-		}
-		gMouseDownX = gMouseX
-		gMouseDownY = gMouseY
-		e.preventDefault()
-		return false
-	}, {passive:false})
-	
-	document.addEventListener("touchmove", e => {
-		for(var touchI=-1,touch; touch=e.changedTouches[++touchI];) {
-			gMouseX = touch.clientX
-			gMouseY = touch.clientY
-		}
-		gMouseMove()
-		e.preventDefault()
-		return false
-	}, {passive:false})
-	
-	document.addEventListener("touchend", e => {
-		gMouseDown = 0
-		gMouseReleased = 1
-		gMouseClicked = !gMouseDragged
-		e.preventDefault()
-		return false
-	}, {passive:false})
-	
-	document.addEventListener("touchcancel", e => {
-		gMouseDown = 0
-		gMouseReleased = 1
-		e.preventDefault()
-		return false
-		
-	}, {passive:false})
-	
-	w.addEventListener("mouseup", e => {
-		gMouseDown = 0
-		gMouseReleased = 1
-		gMouseClicked = !gMouseDragged
-	})
-	
-	onresize()
-	
-	gloop.start(gGameUpdate, gGameDraw)
 }
 
